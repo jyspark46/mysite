7b74d0392943408bab9711190ae6fcb5b85119c6
jelypvid <jyspark46@gmail.com>
2024-06-17 10:33:41 +0900

    Application Context Event & Dynamically Bean Creation

--- mysite3/src/main/java/com/poscodx/mysite/controller/AdminController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/AdminController.java
@@ -2,16 +2,21 @@ package com.poscodx.mysite.controller;
 
 import javax.servlet.ServletContext;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.poscodx.mysite.security.Auth;
+import com.poscodx.mysite.service.FileUploadService;
+import com.poscodx.mysite.service.SiteService;
 import com.poscodx.mysite.vo.SiteVo;
 
-@Auth(role="ADMIN")
 @Controller
+@Auth(role="ADMIN")
 @RequestMapping("/admin")
 public class AdminController {
 	
@@ -21,13 +26,39 @@ public class AdminController {
 	@Autowired
 	private ApplicationContext applicationContext;
 	
+	@Autowired
+	private SiteService siteService;
+
+	@Autowired
+	private FileUploadService fileUploadService;
+	
 	@RequestMapping("")
-	public String main() {
+	public String main(Model model) {
+		SiteVo vo = siteService.getSite();
+		model.addAttribute("siteVo", vo);
+		
 		return "admin/main";
 	}
 	
 	@RequestMapping("/main/update")
-	public String update(SiteVo siteVo) {
+	public String update(SiteVo vo, MultipartFile file) {
+		String profile = fileUploadService.restore(file);
+		System.out.println(profile);
+		if(profile != null) {
+			vo.setProfile(profile);
+		}
+		
+		siteService.updateSite(vo);
+		
+		servletContext.setAttribute("sitevo", vo);
+		
+		SiteVo site = applicationContext.getBean(SiteVo.class);
+//		site.setTitle(vo.getTitle());
+//		site.setWelcome(vo.getWelcome());
+//		site.setProfile(vo.getProfile());
+//		site.setDescription(vo.getDescription());
+		BeanUtils.copyProperties(vo, site); // 위의 4줄을 한 줄로 !!                                           
+		
 		return "redirect:/admin";
 	}
 	
